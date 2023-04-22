26521632a6665f1522a0faf16d87e90342b00d93
Daniel Woodward <16451892+DrBarnabus@users.noreply.github.com>
2023-04-22 23:28:45 +0100

    Install Contentlayer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const { withContentlayer } = require('next-contentlayer');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-  swcMinify: true
-}
+  swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = withContentlayer(nextConfig);
