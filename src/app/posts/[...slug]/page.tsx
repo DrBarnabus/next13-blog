547fd8c27e68b1a2eac82aea74e5aff2167f1b6b
Daniel Woodward <16451892+DrBarnabus@users.noreply.github.com>
2023-04-24 22:53:09 +0100

    Add a new page to render Posts

---
+++ src/app/posts/[...slug]/page.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+import { allPosts } from 'contentlayer/generated';
+
+type Props = {
+  params: {
+    slug: string[];
+  };
+};
+
+export async function generateStaticParams(): Promise<Props['params'][]> {
+  return allPosts.map(({ url }) => ({
+    slug: url.split('/').slice(1), // Remove posts from the start of the url
+  }));
+}
+
+export default function Page({ params }: Props) {
+  const post = allPosts.find((post) => post.url === `/posts/${params.slug.join('/')}`);
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <article className="mx-auto max-w-3xl py-8">
+      <Link href="/" className="text-center font-semibold text-indigo-500">
+        Home
+      </Link>
+
+      <div className="my-6">
+        <h1 className="mb-1 text-3xl font-bold">{post.title}</h1>
+        <time dateTime={post.publishedDate} className="block text-sm text-zinc-600">
+          {post.publishedDate}
+        </time>
+      </div>
+
+      <div dangerouslySetInnerHTML={{ __html: post.body.html }}></div>
+    </article>
+  );
+}
